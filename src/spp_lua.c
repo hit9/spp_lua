00d7917b76fcb6e19c8843f045a7bd4ad05e5bd1
hit9 <hit9@icloud.com>
2015-03-11 19:15:41 +0800

    Compact lua 5.1/5.2/5.3

--- src/spp_lua.c
+++ src/spp_lua.c
@@ -99,8 +99,13 @@ luaopen_spp_lua(lua_State *L)
     luaL_newmetatable(L, "spp_parser");
     lua_pushvalue(L, -1);
     lua_setfield(L, -2, "__index");
+#if LUA_VERSION_NUM == 501
     luaL_register(L, NULL, spp_parser_methods);
     luaL_register(L, "spp_lua", spp_lua_funcs);
+#else
+    luaL_setfuncs(L, spp_parser_methods, 0);
+    luaL_newlib(L, spp_lua_funcs);
+#endif
     return 1;
 }
 #ifdef __cplusplus
